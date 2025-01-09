40439a5b37b54fc565e8b3a3fabb35f070094650
Kevin Cepria <kevin.cepria@gmail.com>
2025-01-09 19:20:15 +0800

    add demo for both AES-GCM and AES-CBC encryption methods

--- index.ts
+++ index.ts
@@ -1,13 +1,36 @@
-import { encryptURL, decryptURL } from "./methods/AES-GCM";
+import {
+  encryptURL as aesGcmEncryptUrl,
+  decryptURL as aesGcmDecryptUrl,
+} from "./methods/AES-GCM";
+
+import {
+  encryptURL as aesCbcEncryptUrl,
+  decryptURL as aesCbcDecryptUrl,
+} from "./methods/AES-CBC";
 
 const url = "https://example.com/page?param=value";
 
 (async () => {
-  // Encrypt the URL
-  const encryptedURL = await encryptURL(url);
-  console.log("Encrypted URL:", encryptedURL);
+  console.log("------------------ENCRYPTION DEMO------------------\n");
+  console.log("-----AES-256(AES-GCM)-----\n");
+
+  console.log("Given URL: ", url, "\n");
+  const aesGcmEncryptedURL = await aesGcmEncryptUrl(url);
+  console.log("Encrypted URL:", aesGcmEncryptedURL, "\n");
 
   // Decrypt the URL
-  const decryptedURL = await decryptURL(encryptedURL);
-  console.log("Decrypted URL:", decryptedURL);
+  const aesGcmDecryptedURL = await aesGcmDecryptUrl(aesGcmEncryptedURL);
+  console.log("Decrypted URL:", aesGcmDecryptedURL, "\n");
+
+  console.log("-----AES-256(AES-CBC)-----\n");
+
+  console.log("Given URL: ", url, "\n");
+  const aesCbcEncryptedURL = await aesCbcEncryptUrl(url);
+  console.log("Encrypted URL:", aesCbcEncryptedURL, "\n");
+
+  // Decrypt the URL
+  const aesCbcDecryptedURL = await aesCbcDecryptUrl(aesCbcEncryptedURL);
+  console.log("Decrypted URL:", aesCbcDecryptedURL, "\n");
+
+  console.log("------------------DEMO END------------------\n");
 })();
